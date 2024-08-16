11ac5a1921eaf0c6ae5c1c4e9eef4777b3a6b203
Ryland Marcus <rylandmarcus@yahoo.com>
2024-08-15 21:40:59 -0700

    leave league

--- src/pages/League.jsx
+++ src/pages/League.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 import Joinleaguebutton from '../components/Joinleaguebutton'
+import axios from 'axios'
 
 const League = () => {
     const league = useLoaderData()
+    const leaveLeague = (e)=>{
+        e.preventDefault()
+        axios.put(process.env.REACT_APP_BACKEND_URL+'/leagues/leaveleague/'+league._id).then(
+            res=>{
+                window.location.href = '/leagues'
+            }
+        )
+        console.log('leave league')
+    }
   return (
     <div>
         <h1>{league.name}</h1>
-        <h2>{league.teams.length}/{league.leagueSize} {league.teams.length==league.leagueSize?"league full": league.currentUser==-1?<Joinleaguebutton league={league}></Joinleaguebutton>:null}</h2>
+        <h2>{league.teams.length}/{league.leagueSize} {league.teams.length==league.leagueSize?"league full": league.currentUser==-1?<Joinleaguebutton league={league}></Joinleaguebutton>:null} {league.currentUser>=0?<button onClick={leaveLeague}>Leave League</button>:null}</h2>
         <h2>Teams:</h2>
         {league.teams.map((team, index)=>{
             return(
