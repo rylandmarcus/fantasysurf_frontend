6418166eacf40ba8781fe3be5d194f7f2e815cff
Ryland Marcus <rylandmarcus@yahoo.com>
2024-08-25 18:50:20 -0600

    more sockets

--- src/pages/Draftroom.jsx
+++ src/pages/Draftroom.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
 
 const Draftroom = () => {
+    const [league, setLeague] = useState({})
     const socket = io.connect(process.env.REACT_APP_BACKEND_URL)
+    useEffect(()=>{
+        socket.on('connect', ()=>{
+            console.log('connected')
+        })
+        return ()=>{
+            //leave room
+        }
+    },[socket])
+    useEffect(()=>{
+        socket.emit('join draftroom', {league: league._id})
+        return ()=>{
+            socket.emit('leave draftroom', {league: league._id})
+        }
+    },[league])
   return (
     <div>Draftroom</div>
   )
