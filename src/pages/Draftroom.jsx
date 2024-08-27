faa3a8f2d692415308ffaae4eb6e6c62646fd851
Ryland Marcus <rylandmarcus@yahoo.com>
2024-08-27 17:46:45 -0600

    brutal socket stuff

--- src/pages/Draftroom.jsx
+++ src/pages/Draftroom.jsx
@@ -6,6 +6,7 @@ const Draftroom = () => {
     const league = useLoaderData()
     const [leagueCopy, setLeagueCopy] = useState(league)
     const socket = io.connect(process.env.REACT_APP_BACKEND_URL)
+    const [announcementBar, setAnnouncementBar] = useState('')
     useEffect(()=>{
         socket.on('connect', ()=>{
             console.log('connected')
@@ -16,6 +17,26 @@ const Draftroom = () => {
         socket.on('receiveHiOthers', ()=>{
             console.log('hello from another user in the draft room')
         })
+        // socket.on('thePickIsIn', ()=>{
+        //     console.log('the pick is in')
+        //     setAnnouncementBar('The Pick Is In')
+        // })
+        socket.on('receiveDraft', (draftedSurferId, drafterIdx)=>{
+            console.log('received draft')
+            console.log(draftedSurferId)
+            if (draftedSurferId){
+                const draftedSurferIdx = league.event.surfers.findIndex(surfer=>surfer._id === draftedSurferId)
+                if (draftedSurferIdx>=0){     
+                    let updatedLeague = JSON.parse(JSON.stringify(leagueCopy)); // Deep clone
+                    console.log(updatedLeague.event.surfers)
+                    updatedLeague.teams[drafterIdx].surfers.push(draftedSurferIdx)
+                    updatedLeague.event.surfers = updatedLeague.event.surfers.filter(surfer=>surfer._id !== draftedSurferId)
+                    setLeagueCopy(updatedLeague)
+                    setAnnouncementBar('')
+                    console.log(league.event.surfers[draftedSurferIdx].name+' was drafted by '+league.teams[drafterIdx].name)
+                }
+            }
+        })
         return ()=>{
             //leave room
         }
@@ -25,7 +46,8 @@ const Draftroom = () => {
         return ()=>{
             socket.emit('leave draftroom', league._id)
         }
-    },[league])
+    },[leagueCopy])
+    //^This used to be league not leagueCopy and it was rerendering league causing the copies to reset, now we hop in and out of the room each pick, idk if this is still the right answer but it seems to be work atm
     const sayHi = () => {
         socket.emit('sayHi', leagueCopy._id)
     }
@@ -35,13 +57,13 @@ const Draftroom = () => {
     const draftSurfer = (e) => {
         const draftedSurferId = leagueCopy.event.surfers[e.target.previousSibling.previousSibling.innerText-1]._id
         console.log('drafting '+e.target.previousSibling.previousSibling.innerText)
-        socket.emit('draftSurfer', draftedSurferId, league.currentUser)
-        //YOU ARE HERE
+        socket.emit('draftSurfer', draftedSurferId, league.currentUser, league._id)
     }
   return (
     <div>
         <h1>Draftroom</h1>
         <h2>Draft for {leagueCopy.name}</h2>
+        <h2>{announcementBar}</h2>
         <button onClick={sayHi}>Say Hi</button>
         <button onClick={hiOthers}>Say Hi To Others</button>
         <h3>Teams</h3>
@@ -50,7 +72,8 @@ const Draftroom = () => {
                 <h4>{team.name}</h4>
                 <div>
                     {team.surfers.map((surfer, i)=>{
-                        return <li key={surfer._id+'in teams'+i}>{surfer.name}</li>
+                        // const s = league.event.surfers[surfer]
+                        return <li key={league.event.surfers[surfer]._id+'in teams'+i}>{league.event.surfers[surfer].name}</li>
                     })}
                 </div>
             </div>
